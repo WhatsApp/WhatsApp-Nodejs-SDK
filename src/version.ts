5e0dd529011ff64f29aeecfd8a9c8b5c99e2f90e
Rashed Talukder <rashed@fb.com>
2023-04-11 16:27:29 -0700

    Bump version

--- src/version.ts
+++ src/version.ts
@@ -8,4 +8,4 @@
 
 import { semanticVersionString } from './types/version';
 
-export const SDKVersion: semanticVersionString = '0.0.4-Alpha';
+export const SDKVersion: semanticVersionString = '0.0.5-Alpha';
